8f950f4c5a62cb5c6630aa73d2f688aeccb05809
ahfpca <ahfpca@gmail.com>
2018-05-15 15:50:43 -0700

    assignment done

---
+++ apps/surveys/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, HttpResponse, redirect
+
+# Create your views here.
+def index(request):
+    return render(request, "surveys/index.html")
+
+def process(request):
+    if request.method == "POST":
+        request.session["name"] = request.POST["name"]
+        request.session["location"] = request.POST["location"]
+        request.session["language"] = request.POST["language"]
+        request.session["comment"] = request.POST["comment"]
+        return redirect("/result")
+    
+    return redirect("/")
+
+def result(request):
+    return render(request, "surveys/result.html")
